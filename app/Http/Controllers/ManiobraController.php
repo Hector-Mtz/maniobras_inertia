41351e0386056c85f6e981b42b13fc7b706df43a
Hector Hugo Martinez Martinez <57997735+hh-mm77@users.noreply.github.com>
2022-05-25 08:26:02 -0500

    comentario

--- app/Http/Controllers/ManiobraController.php
+++ app/Http/Controllers/ManiobraController.php
@@ -116,7 +116,7 @@ class ManiobraController extends Controller
             $montos= monto::where('turno_id','like','%'.$turnoSelect.'%')->get();
         }
 
-
+ ///comentario
         return Inertia::render('Maniobras',[
             'cedis'=> $cedis,
             'clientes' =>$clientes,
