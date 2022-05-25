f27d08bf1f470b48af5bff98e206d55ade56c159
Hector Hugo Martinez Martinez <57997735+hh-mm77@users.noreply.github.com>
2022-05-25 17:17:47 -0500

    generacion de reportes casi lista

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,6 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified',
     //Route::apiResource('/lista_asistencia', ListaAsistenciaController::class);
     Route::get('/lista_asistencia/export/', [ListaAsistenciaController::class, 'export'])->name('lista_asistencia.export');
 
-  //hola
 
 
     Route::get('/mapa', function () {
